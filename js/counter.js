a1780ab2458338bb9865a968561cd4924c0be010
Oleg Kitaev <kitaev_oleg@mail.ru>
2024-03-07 01:45:48 +0300

    extend js counter to all items

--- js/counter.js
+++ js/counter.js
@@ -1,17 +1,38 @@
-const minusBtn = document.querySelector('[data-action="minus"]');
-const plusBtn = document.querySelector('[data-action="plus"]');
-const counter = document.querySelector("[data-counter]");
+window.addEventListener("click", function (event) {
+  let counter;
+  const eventPlus = event.target.dataset.action === "plus";
+  const eventMinus = event.target.dataset.action === "minus";
 
-console.log(minusBtn, plusBtn, parseInt(counter.innerText));
+  if (eventPlus || eventMinus) {
+    const counterWrapper = event.target.closest(".counter-wrapper");
+    counter = counterWrapper.querySelector("[data-counter]");
+  }
 
-minusBtn.addEventListener("click", function () {
-  console.log("minus");
-  if (parseInt(counter.innerText) > 1) {
-    --counter.innerText;
+  if (eventPlus) {
+    ++counter.innerText;
+  }
+
+  if (eventMinus) {
+    if (parseInt(counter.innerText) > 1) {
+      --counter.innerText;
+    }
   }
 });
 
-plusBtn.addEventListener("click", function () {
-  console.log("plus");
-  ++counter.innerText;
-});
+// const minusBtn = document.querySelector('[data-action="minus"]');
+// const plusBtn = document.querySelector('[data-action="plus"]');
+// const counter = document.querySelector("[data-counter]");
+
+// console.log(minusBtn, plusBtn, parseInt(counter.innerText));
+
+// minusBtn.addEventListener("click", function () {
+//   console.log("minus");
+//   if (parseInt(counter.innerText) > 1) {
+//     --counter.innerText;
+//   }
+// });
+
+// plusBtn.addEventListener("click", function () {
+//   console.log("plus");
+//   ++counter.innerText;
+// });
